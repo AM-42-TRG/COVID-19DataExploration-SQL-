dc00a1a5faf04ef14b32ab86b21c001a55d03d64
AM-42-TRG <116633371+AM-42-TRG@users.noreply.github.com>
2022-10-25 11:09:51 +0200

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -1,21 +1,7 @@
--- Tutorial URL: https://youtu.be/qfyynHBFOsM
-
 /*
 Portfolio Project by Alex Freberg:
 SQL Data Exploration
 */
-
-select *
-from PortfolioProject.dbo.CovidDeaths
-
-select *
-from PortfolioProject.dbo.CovidVaccinations
-
---Select data that we are going to be using
-
-select location, date, total_cases, new_cases, total_deaths, population
-from PortfolioProject.dbo.CovidDeaths
-order by 1, 2
 
 
 -- Looking at Total Cases vs Total Deaths
@@ -33,7 +19,6 @@ where location like '%states%'
 order by 1, 2
 
 -- Looking at countries with highest infection rate compared to population
-
 select location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_deaths/population))*100 as PercentPopulationInfected
 from PortfolioProject.dbo.CovidDeaths
 --where location like '%states%'
@@ -48,7 +33,6 @@ where continent is not null
 group by location, population
 order by TotalDeathCount desc
 
-
 -- Showing the continents with the highest death count
 select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 from PortfolioProject.dbo.CovidDeaths
@@ -56,10 +40,6 @@ from PortfolioProject.dbo.CovidDeaths
 where continent is not null
 group by continent
 order by TotalDeathCount desc
-
-
-
-
 
 
 
@@ -71,28 +51,7 @@ where continent is not null
 group by date
 order by 1, 2
 
-
--- GLOBAL NUMBERS2
-select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
-from PortfolioProject.dbo.CovidDeaths
--- where location like '%states%'
-where continent is not null
---group by date
-order by 1, 2
-
--- Video at 50:00
--- https://www.youtube.com/watch?v=qfyynHBFOsM
-
--- Joining the two tables (Join = Inner Join)
-
-SELECT *
-FROM PortfolioProject..CovidDeaths dea -- 'dea' is the alias
-Join PortfolioProject..CovidVaccinations vac -- 'vac' is the alias
-	ON dea.location = vac.location
-	AND dea.date = vac.date
-
 -- Looking at Total Population vs Vaccinations
-
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 FROM PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
@@ -100,10 +59,6 @@ Join PortfolioProject..CovidVaccinations vac
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3
-
--- Video at 56:11
-
--- Video at 1:01:23
 
 -- Doing a rolling count
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -179,11 +134,7 @@ Join PortfolioProject..CovidVaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
--- ORDER BY 2,3
 
--- Viewing the view
+
 SELECT *
 FROM PercentPopulationVaccinated
-
--- https://www.sqlservertutorial.net/sql-server-views/
--- "A view is a named query stored in the database catalog that allows you to refer to it later."
